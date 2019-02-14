9a70d6f72ff3b52aaecc36fc4ea18a293208ceec
flance <flance85@163.com>
2019-02-14 22:11:09 +0800

    提交一个ftp工具，实现ftp的文件上传、下载功能

---
+++ src/test/java/com/github/flance/util/network/FtpUtilsTest.java
@@ -0,0 +1,22 @@
+package com.github.flance.util.network;
+
+import org.junit.Test;
+
+public class FtpUtilsTest {
+
+    @Test
+    public void getFTPClient() {
+    }
+
+    @Test
+    public void closeFTPClient() {
+    }
+
+    @Test
+    public void download() {
+    }
+
+    @org.junit.Test
+    public void upload() {
+    }
+}
